c2490b291ad80953c3192829d622dfa7f415e7ca
Nigel Perks <nigelperks.github@gmail.com>
2023-11-26 17:28:38 +0000

    Reformat test.py

--- test.py
+++ test.py
@@ -5,9 +5,11 @@ import sys
 from glob import glob
 import filecmp
 
+
 def fatal(msg):
   print("FATAL:", msg)
   sys.exit(1)
+
 
 def banner(s):
   b = "*" * (len(s) + 4)
@@ -15,11 +17,13 @@ def banner(s):
   print("*", s, "*")
   print(b)
 
+
 def check_cmd(cmd):
   r = os.system(cmd)
   if r != 0:
     print("Command failed: exit code", r)
     sys.exit(1)
+
 
 def check_output(source, typ):
   if not source.endswith(".bas"):
@@ -40,21 +44,23 @@ def check_output(source, typ):
         print("ERROR: no output was expected")
         offer_new_reference(ref, typ)
 
+
 def offer_new_reference(ref, typ):
-        print("--------")
-        with open(typ) as f:
-          line = f.readline()
-          while line != "":
-            print(line,end="")
-            line = f.readline()
-        print("--------")
-        print("Make new reference?")
-        if input().startswith("y"):
-          if os.path.isfile(ref):
-            os.remove(ref)
-          check_cmd("copy " + typ + " " + ref)
-        else:
-          sys.exit(1)
+  print("--------")
+  with open(typ) as f:
+    line = f.readline()
+    while line != "":
+      print(line,end="")
+      line = f.readline()
+  print("--------")
+  print("Make new reference?")
+  if input().startswith("y"):
+    if os.path.isfile(ref):
+      os.remove(ref)
+    check_cmd("copy " + typ + " " + ref)
+  else:
+    sys.exit(1)
+
 
 def test(exe, source):
   banner(source)
@@ -69,6 +75,7 @@ def test(exe, source):
   check_output(source, "out")
   check_output(source, "err")
   print("PASS")
+
 
 # MAIN
 
